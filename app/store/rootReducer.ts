d3df61c29983542f034db457817ab963b24dd1e0
yevhencheshchevyi <zhenka.go@gmail.com>
2021-07-06 09:41:04 +0300

    clear all store #LUN-162

--- app/store/rootReducer.ts
+++ app/store/rootReducer.ts
@@ -1,7 +1,7 @@
 // redux
 import { combineReducers } from '@reduxjs/toolkit'
 // reducers
-import auth from 'store/auth/model'
+import auth, { clearAuth } from 'store/auth/model'
 import foodCategories from 'store/foodCategories/model'
 import trucks from 'store/trucks/model'
 import foodTypes from 'store/foodTypes/model'
@@ -9,7 +9,7 @@ import general from 'store/general/model'
 import orders from 'store/orders/model'
 import payments from 'store/payments/model'
 
-const rootReducer = combineReducers({
+const combineReducer = combineReducers({
   auth,
   foodCategories,
   trucks,
@@ -19,4 +19,11 @@ const rootReducer = combineReducers({
   payments,
 })
 
+const rootReducer: typeof combineReducer = (state, action) => {
+  if (action.type === clearAuth.type) {
+    return combineReducer(undefined, action)
+  }
+
+  return combineReducer(state, action)
+}
 export default rootReducer
